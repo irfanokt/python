c6bb804423a5826ee119d1c8a7620edf95fce086
irfanokt <118596332+irfanokt@users.noreply.github.com>
2022-12-12 06:48:47 +0700

    Membuat variabel di python

---
+++ variabel.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 05:18:30 2022
+
+@author: Irfan
+"""
+
+# Variabel adalah tempat menyimpan data
+# Menaruh / assignment nilai
+
+a = 10
+x = 5
+panjang = 1000
+
+# Pemanggilan pertama
+print('nilai a = ', a)
+print('nilai x = ', x)
+print('nilai panjang = ', panjang)
+
+# Penamaan
+
+nilai_y = 15 # Menggunakan underscore
+juta10 = 10000000 # Ini boleh
+nilaiZ = 17.5 # Ini boleh
+
+# Pemanggilan kedua
+print('nilai a = ', a)
+a = 7
+print('nilai a = ', a)
+# assignment indirect
+b = a
+print('nilai b = ', b)
